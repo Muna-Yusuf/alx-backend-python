067269a952d88f17edcd1c6e613c314cb20391b0
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-06 21:23:15 +0000

    ...

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""DOC"""
+"""Augment the following code with the correct duck-typed annotations"""
 
 
 from typing import Sequence, Any, Union
@@ -10,4 +10,4 @@ def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
-        return None
+        return Nong
