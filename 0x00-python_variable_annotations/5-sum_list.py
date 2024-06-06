3aa669a5136aa878a5cf42140eee0c45cbb52a3d
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-06 21:19:30 +0000

    ...

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,13 @@
 """function sum_list which takes a list input_list of
    floats as argument and returns their sum as a float."""
 
+
 from typing import List
 
 
-gef sum_list(input_list: List[float]) -> float:
-    '''Outputs sum of all elements in input_list. '''
+def sum_list(input_list: List[float]) -> float:
+    """
+    Typed-annotated function
+    sum_list
+    """
     return sum(input_list)
