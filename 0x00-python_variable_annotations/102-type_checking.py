a676529ddaf764e3c5ba2475c79d1e30a91d6e26
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-06 21:29:08 +0000

    ...

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
-"""Use mypy to validate the following piece of
-   code and apply any necessary changes."""
+"""
+Given the parameters and the return values,
+add type annotations to the function
 
-
-from typing import Union, Any, Mapping, Tuple, List
+Hint: look into TypeVar
+"""
+from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    ''' Variable Annotation for list '''
+    """
+    Return necessary changes
+    """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
-    ]
+        ]
     return zoomed_in
 
 
-array = tuple([12, 72, 91])
+array: Tuple = (12, 72, 91)
+
 zoom_2x = zoom_array(array)
-zoom_3x = zoom_array(array, int(3.0))
+
+zoom_3x = zoom_array(array, 3)
