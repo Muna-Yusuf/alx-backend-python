6b309343a3056180fb97535e445e5d2e25f40f37
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-06 21:17:30 +0000

    ...

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""DOC"""
+"""function sum_list which takes a list input_list of
+   floats as argument and returns their sum as a float."""
 
 from typing import List
 
 
-def sum_list(input_list: List[float]) -> float:
+gef sum_list(input_list: List[float]) -> float:
     '''Outputs sum of all elements in input_list. '''
     return sum(input_list)
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""DOC"""
+"""function sum_mixed_list which takes a list mxd_lst of
+   integers and floats and returns their sum as a float."""
 
 
 from typing import List, Union
