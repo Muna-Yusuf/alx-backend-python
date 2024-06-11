4f1afe87c852fb25732a2fe5fe2778e22be842b8
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-11 10:09:57 +0000

    fixing 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 """Import async_generator from the previous task and
-   then write a coroutine called async_comprehension 
+   then write a coroutine called async_comprehension
    that takes no arguments."""
 
-
-async_generator = __import__('0-async_generator.py').async_generator
-
-import asyncio
+import asynico
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
