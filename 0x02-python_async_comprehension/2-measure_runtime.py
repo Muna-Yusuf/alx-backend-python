c671dbf14a9fbbcaadd5d3f813d67738f72fee53
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-11 10:56:30 +0000

    ...

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -12,11 +12,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """ the a measure_runtime coroutine."""
     start_time = time()
-    await gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await gather(async_comprehension(), async_comprehension(),
+                 async_comprehension(), async_comprehension())
     end_time = time()
     return end_time - start_time
