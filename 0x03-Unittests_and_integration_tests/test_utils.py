79132afad24e658ed69faa3147f6e6442c194546
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-07-01 08:34:33 +0000

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Test SUITE Unittest module Task """
+"""Unittests and Integration Tests Tasks."""
 
 from unittest import TestCase, mock
 from unittest.mock import patch, Mock
@@ -8,14 +8,14 @@ from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(TestCase):
-    """ Class for testing Nested Map function """
+    """Utils.access_nested_map functions."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, map, path, expected_output):
-        """ Test method return output """
+        """Test Method."""
         real_output = access_nested_map(map, path)
         self.assertEqual(real_output, expected_output)
 
@@ -24,34 +24,30 @@ class TestAccessNestedMap(TestCase):
         ({"a": 1}, ("a", "b"), 'b')
     ])
     def test_access_nested_map_exception(self, map, path, wrong_output):
-        """ Test method raises correct exception """
+        """ Test Method."""
         with self.assertRaises(KeyError) as e:
             access_nested_map(map, path)
             self.assertEqual(wrong_output, e.exception)
 
 
 class TestGetJson(TestCase):
-    """ Class for testing get_json function """
-    # order of args: test_url, test_payload
+    """Get_json functions."""
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
-        """ Test method returns correct output """
-        # set mock response to have return value of test payload
+        """ Test Method."""
         mock_response = Mock()
         mock_response.json.return_value = test_payload
-        # function calls requests.get, need patch to get mock return value
         with patch('requests.get', return_value=mock_response):
             real_response = get_json(test_url)
             self.assertEqual(real_response, test_payload)
-            # check that mocked method called once per input
             mock_response.json.assert_called_once()
 
 
 class TestMemoize(TestCase):
-    """ Class for testing memoization """
+    """Utils.memoize functions."""
 
     def test_memoize(self):
         """ Tests memoize function """
